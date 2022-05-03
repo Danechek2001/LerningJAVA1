de8555fc7dbbbea72a5f5de6e417ced0d4dbf41f
Your Name <you@example.com>
2022-05-04 00:15:38 +0300

    Удалил обитателей

--- src/zolax_lox/Main.java
+++ src/zolax_lox/Main.java
@@ -1,6 +1,5 @@
 package zolax_lox;
 
-import zolax_lox.Obitatel;
 
 import java.util.Arrays;
 import java.util.Random;
@@ -9,10 +8,6 @@ import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-        Obitatel a = new Obitatel("", 0, "", false);
-        Obitatel b = new Obitatel("", 0, "", true);
-        Obitatel c = new Obitatel("", 0, "", false);
-        a.printParen();
 
     }
 }
